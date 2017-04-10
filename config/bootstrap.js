5f1b92ef131e2989c49bbfd6e053a7535a430e1c
Raymon Johnstone <johnstor@gmail.com>
2017-04-10 20:54:35 +0000

    This heartbeat implementation is likely all wrong...I am gonna try and roll with it anyway.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,7 +8,18 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
+var heartbeats = require('heartbeats');
 
 module.exports.bootstrap = function(cb) {
-        cb();
+        
+// a heart that beats every 1 second. 
+var heart = heartbeats.createHeart(1000);
+
+heart.createEvent(5, function(count, last){
+  console.log('...Every 5 Beats forever');
+});
+
+
+cb();
+
 };
