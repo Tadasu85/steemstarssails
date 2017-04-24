982f5b499abed00f975fe8d04c6ff9e133de1961
Raymon Johnstone <johnstor@gmail.com>
2017-04-24 06:02:17 +0000

    Successful refactor to seperate app from UI created new file for UI, also successfuly migrated to new app meny lib cxtmenu, I think it looks really nice.

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,7 @@
 var steemaccount = "";
 var cy;
 
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
 if (window.location.pathname=='/permission/galaxy') {
@@ -115,97 +116,9 @@ var cy = window.cy = cytoscape({
             classes: 'background'
               }]
 });
-var cxtmenuApi = cy.cxtmenu(defaults);
-var MenuOne = cy.contextMenus({
-menuItems: [
-{
-    id: 'viewonsteem',
-    title: 'View On Steem',
-    selector: 'node',
-    onClickFunction: function (event) {
-var selectedNode = event.cyTarget.attr("label");
-    window.open('https://steemit.com/@'+selectedNode,'_blank');
-    }
-  },
-  {
-    id: 'buildmenu',
-    title: 'Build Menu',
-    selector: '.parent',
-    onClickFunction: function (event) {
-    selectedNode = event.cyTarget.attr("id");
-    $("#jigonsaseh-data").html("<center>" +event.cyTarget.attr("id").toUpperCase()+"</center>");
-    document.getElementById('jigonsaseh').style.display='block';
-    document.getElementById('fade').style.display='block';
-  }
-
-  },
-  {
-    id: 'viewjigonsaseh',
-    title: 'View Jigonsaseh',
-    selector: '.mutual, .followers, .follows', 
-    onClickFunction: function (event) {
-    
-    $("#jigonsaseh-data").html("<center>" +event.cyTarget.attr("id").toUpperCase()+"</center>");
-    document.getElementById('jigonsaseh').style.display='block';
-    document.getElementById('fade').style.display='block';
-    }
-  },
-  {
-    id: 'viewlenged',
-    title: 'View Legend',
-    selector: '', 
-    coreAsWell: true,
-    onClickFunction: function () {
-    document.getElementById('legend').style.display='block';
-    document.getElementById('fade').style.display='block';
-    }
-  },
-],
-menuItemClasses: ['custom-menu-item'],
-contextMenuClasses: ['custom-context-menu']
-});
-$('#closelegend').on("click", function closelegend(event) {
-document.getElementById('legend').style.display='none';
-document.getElementById('fade').style.display='none';
-});
-$('input#closejigonsaseh').on("click", function closelegend(event) {
-document.getElementById('jigonsaseh').style.display='none';
-document.getElementById('fade').style.display='none';
-});
-configureHUD();
-//addFollowers();
-//addFollows();
 testing();
 //setTimeout(function(){ cy.layout({name: 'preset', stop: function(){}});  }, 5000);
 
-var defaults = {
-  menuRadius: 100, // the radius of the circular menu in pixels
-  selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
-  commands: [ // an array of commands to list in the menu or a function that returns the array
-    
-    /*{ // example command
-      fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
-      content: 'a command name' // html/text content to be displayed in the menu
-      select: function(ele){ // a function to execute when the command is selected
-        console.log( ele.id() ) // `ele` holds the reference to the active element
-      }
-    }*/
-    
-  ], // function( ele ){ return [ /*...*/ ] }, // example function for commands
-  fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
-  activeFillColor: 'rgba(92, 194, 237, 0.75)', // the colour used to indicate the selected command
-  activePadding: 20, // additional size in pixels for the active command
-  indicatorSize: 24, // the size in pixels of the pointer to the active command
-  separatorWidth: 3, // the empty spacing in pixels between successive commands
-  spotlightPadding: 4, // extra spacing in pixels between the element and the spotlight
-  minSpotlightRadius: 24, // the minimum radius in pixels of the spotlight
-  maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight
-  openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
-  itemColor: 'white', // the colour of text in the command's content
-  itemTextShadowColor: 'black', // the text shadow colour of the command's content
-  zIndex: 9999, // the z-index of the ui div
-  atMouse: false // draw menu at mouse position
-};
 }
 });
 function addFollowers(){
@@ -265,51 +178,18 @@ steem.api.getFollowing(ele.id(), 0, "blog", 100, function(err, result) {
     cy.endBatch();
 }
 
-function configureHUD(){
-    var globalJigs;
-    var globalTime;
-    var globalResearch = 0;
-    var globalPower = 0;
-    var globalMaterials = 0;
-    var globalPopulation = 0;
-    var globalShips = 0;
-    var globalEmbassies = 0;
-    io.socket.get('/population/?user=' + steemaccount, function gotResponse(body, response){
-        //console.log(body, response);
-        //console.log(steemaccount);
-        globalPopulation = body[0].amount;
-        //console.log(globalPopulation);
-    });
-    
-setTimeout(function(){
-    $("#global-hud-top").html("Research: " + globalResearch+" Power: "+globalPower+" Materials: "+globalMaterials+
-" Population: "+globalPopulation+" Ships: "+globalShips+" Embassies: "+globalEmbassies);
-//console.log(globalPopulation);
-}, 1000);
 
-steem.api.getAccountCount(function(err, result) {
-    globalJigs = result;
-    //console.log(err, result);
-    $("#global-hud-bottom").html("Global Accounts: " + globalJigs);
-});
-steem.api.getDynamicGlobalProperties(function(err, result) {
-    globalTime = result.time;
-     //console.log(err, result);
-     $("#global-hud-bottom").append(" Current Block Time: " + globalTime );
-});
-
-}
 
 function testing(){
     function allDone(notAborted, arr) {
-        console.log("done", notAborted, arr);
+        //console.log("done", notAborted, arr);
         for(var obj = 0; obj<arr.length;obj++){
-            console.log(arr[obj].name);
+            //console.log(arr[obj].name);
             cy.add({group: "nodes", data: {id: arr[obj].name, label: arr[obj].name}, position: {x: parseFloat(arr[obj].x_coord), y: parseFloat(arr[obj].y_coord)}});
         }
         cy.layout({name: 'preset', stop: function(){}});
     }
-    io.socket.get('/planet', {limit: 10000}, function(things, jwr) 
+    io.socket.get('/planet', {limit: 10}, function(things, jwr) 
     {
     forEach(things, function(item, index, arr) {
     //console.log("each", item, index, arr);
---
+++ assets/js/userinterface.js
@@ -0,0 +1,98 @@
+var steemaccount = "";
+document.addEventListener("DOMContentLoaded", function(event) {
+if (window.location.pathname=='/permission/galaxy') {
+//Fire and attach CXT menu and items to cytoscape here, define buttons so on.
+    cy.cxtmenu({
+	selector: 'node',
+	commands: [
+		{
+		content: 'View On Steem',
+		select: function(ele)
+			{
+			var selectedNode = ele.data('label');
+            window.open('https://steemit.com/@'+selectedNode,'_blank');
+            }
+		},
+		{
+		content: 'Build Menu',
+		select: function(ele){
+			if(ele.data('label') == steemaccount){
+			    $("#jigonsaseh-data").html("<center>" +steemaccount.toUpperCase()+"</center>");
+                document.getElementById('jigonsaseh').style.display='block';
+                document.getElementById('fade').style.display='block';
+			        }
+			else{
+			    alert("This is not your home!");
+			}
+				},
+			disabled: false
+		},
+		{
+		content: 'View Legend',
+		select: function(ele){
+			    document.getElementById('legend').style.display='block';
+                document.getElementById('fade').style.display='block';
+		            }
+		},
+		{
+		content: 'View Planet',
+		select: function(ele){
+			$("#jigonsaseh-data").html("<center>" +ele.data("label").toUpperCase()+"</center>");
+            document.getElementById('jigonsaseh').style.display='block';
+            document.getElementById('fade').style.display='block';
+		        }
+		}]
+	});
+    cy.cxtmenu({
+	    selector: 'core',
+	    commands: [
+	        {
+	            content: 'View Legend',
+		select: function(ele){
+			    document.getElementById('legend').style.display='block';
+                document.getElementById('fade').style.display='block';
+		      }
+        }]
+    });
+$('#closelegend').on("click", function closelegend(event) {
+document.getElementById('legend').style.display='none';
+document.getElementById('fade').style.display='none';
+});
+$('input#closejigonsaseh').on("click", function closelegend(event) {
+document.getElementById('jigonsaseh').style.display='none';
+document.getElementById('fade').style.display='none';
+});
+
+//Setup outer UI IE top and bottom heads up displays.
+	
+configureHUD();
+    }
+});
+
+function configureHUD(){
+    var globalJigs;
+    var globalTime;
+    var globalResearch = 0;
+    var globalPower = 0;
+    var globalMaterials = 0;
+    var globalPopulation = 0;
+    var globalShips = 0;
+    var globalEmbassies = 0;
+io.socket.get('/population/?user=' + steemaccount, function gotResponse(body, response){
+    globalPopulation = body[0].amount;
+});
+    
+setTimeout(function(){
+$("#global-hud-top").html("Research: " + globalResearch+" Power: "+globalPower+" Materials: "+globalMaterials+
+" Population: "+globalPopulation+" Ships: "+globalShips+" Embassies: "+globalEmbassies);
+}, 500);
+
+steem.api.getAccountCount(function(err, result) {
+globalJigs = result;
+$("#global-hud-bottom").html("Global Accounts: " + globalJigs);
+});
+steem.api.getDynamicGlobalProperties(function(err, result) {
+globalTime = result.time;
+$("#global-hud-bottom").append(" Current Block Time: " + globalTime );
+});
+    }
