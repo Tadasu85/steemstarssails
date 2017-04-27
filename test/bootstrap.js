dae0ab15ffd36b281a3a437acadd999f45b66344
Raymon Johnstone <johnstor@gmail.com>
2017-04-27 16:21:33 +0000

    Lifting was failing due to it taking too long on tests, increased from 5000 to 10000 on the timeout for before_all and now tests pass again, silly things.

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -7,7 +7,7 @@ global.should = chai.should();
 before(function (done) {
 
 // Increase the Mocha timeout so that Sails has enough time to lift.
-this.timeout(5000);
+this.timeout(10000);
 
 sails.lift({
    log: {
