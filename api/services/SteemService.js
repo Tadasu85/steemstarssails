695f4c96078817a295cd5949243a89350bd8ec5f
Raymon Johnstone <johnstor@gmail.com>
2017-05-26 22:15:28 -0400

    Maybe I think too much, but iwanted to get these changed commited, now using underscore for _.range in steemservice hopefully this does not create a conflict with lodash.

--- api/services/SteemService.js
+++ api/services/SteemService.js
@@ -1,30 +1,34 @@
 /**
 * SteemService
 *
-* @description :: TODO: Service that handles account creation on steem as well as follows/unfollows, this information will
+* @description :: TODO: Service that watches account creation on steem as well as follows/unfollows, this information will
 * then be packed into the DB.
 */
 var steem = require('steem');
 
 var asyncLoop = require('node-async-loop');
 
+var _ = require('underscore');
+
 var array = [];
 
-
+steem.api.getDynamicGlobalProperties(function(err, result) {
+  	console.log(err, result);
+  	var N = result.last_irreversible_block_num;
+  	array = _.range(1092, N - 1092);
+  	
+  	
+  	console.log(array);
+  	
+	});
 
 setTimeout(function(){
 	sails.log("Hello its me SteemService!"); 
 
-	steem.api.getDynamicGlobalProperties(function(err, result) {
-  	console.log(err, result.last_irreversible_block_num);
-  	array = Array(result.last_irreversible_block_num).fill(0);
-  	console.log(array);
-	});
-
-	asyncLoop(array, function (item, next)
+	asyncLoop(array, function(item, next)
 	{
     steem.api.getBlock(item, function (err, result) {
-    	console.log(result);
+    	console.log(item);
     });
     next();
 	}, function ()
