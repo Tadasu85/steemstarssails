4a6966da076782c994cb0bb9e3c2482edbef4b76
Raymon Johnstone <johnstor@gmail.com>
2017-06-11 14:43:40 -0400

    We are now successfully listening to follow OPS on the steem blockchain and minting those OPS into edges in the steemstars DB. This is only the start of what we need here but it is a good start.

--- api/services/SteemService.js
+++ api/services/SteemService.js
@@ -8,16 +8,29 @@ var steem = require('steem');
 
 module.exports = {
 
-  init: function(options, done) {
+  init: function() {
 
   steem.api.streamTransactionsAsync(function(err, t) {
   	if(err) {
   		return err;
   	};
-  	if(t.operations[0][0] == "custom_json") {
-  		//sails.log(t.operations[0])
+  	if(t.operations[0][0] == "custom_json" && t.operations[0][1].json[2] == 'f') {
+      var currenOperation = JSON.parse(t.operations[0][1].json);
+  		sails.log(currenOperation[1].follower + " to: " + currenOperation[1].following + " through: " + currenOperation[1].what );
+      if(currenOperation[1].what == "blog") {
+        Edge.create({'from':currenOperation[1].follower, 'to':currenOperation[1].following, 'owner':"58dc05bdd6c3d89b075f9cc9"}).exec(function afterwards(err, edge){
+          if (err) {
+                    sails.log(err);
+                    } else {
+                    sails.log(edge);
+                    
+                    };
+        });
+        }else{
+          //TODO: HANDLE UNFOLLOW WITH DELETE EDGE IF IT EXISTS
+        }
   	};
-  	if(t.operations[0][0] == "account_create_with_delegation" && "account_create") {
+  	if(t.operations[0][0] == "account_create_with_delegation") {
   		//sails.log(t.operations[0])
   	};
     
