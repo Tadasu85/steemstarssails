f9fa1f1ceb982e9548bfb39b67a349395f574136
Raymon Johnstone <johnstor@gmail.com>
2017-06-26 19:16:58 -0400

    I have been reading up a little bit a about threejs and it does have some really sweet functionhs within, cytoscape has unfortunately started to feel very bulky for what I am using it for....I may switch the front end out in favor of being about to do full 3d within the browser and simply have ne planet generated with an object of cart coords for all three axis.

---
+++ assets/js/testing.js
@@ -0,0 +1,42 @@
+var scene, camera, renderer;
+var geometry, material, mesh;
+ 
+init();
+animate();
+
+
+ 
+function init() {
+ 
+    scene = new THREE.Scene();
+ 
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+ 
+    geometry = new THREE.CircleGeometry( 200, 200, );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+ 
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+ 
+    document.body.appendChild( renderer.domElement );
+
+
+ 
+}
+ 
+function animate() {
+
+    
+ 
+    requestAnimationFrame( animate );
+ 
+    mesh.rotation.x += 0.1;
+    mesh.rotation.y += 0.0;
+ 
+    renderer.render( scene, camera );
+ 
+}
